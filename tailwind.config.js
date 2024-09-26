e5be83613fa608478a39f95a694d32cf8c436670
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-09-26 20:49:27 +0900

    chore: install dependencies, setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: { extend: {} },
+	plugins: [forms],
+	future: { hoverOnlyWhenSupported: true }
+};
