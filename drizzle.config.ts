533911715fff5d74c028ee203e708aa622f026e1
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-09-28 14:41:48 +0900

    chore: add drizzle kit configuration files

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'drizzle-kit';
+import { env, loadEnvFile } from 'node:process';
+
+loadEnvFile('.env.development');
+
+if (!env.SQLITE_PATH) throw new TypeError('SQLITE_PATH is undefined');
+
+export default defineConfig({
+	dialect: 'sqlite',
+	schema: './src/lib/server/database/schema.js',
+	dbCredentials: { url: env.SQLITE_PATH }
+});
---
+++ drizzle.production.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'drizzle-kit';
+import { env, loadEnvFile } from 'node:process';
+
+loadEnvFile('.env.production');
+
+if (!env.SQLITE_PATH) throw new TypeError('SQLITE_PATH is undefined');
+
+export default defineConfig({
+	dialect: 'sqlite',
+	schema: './src/lib/server/database/schema.js',
+	dbCredentials: { url: env.SQLITE_PATH }
+});
