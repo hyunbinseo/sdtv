edf20abf8eebdac2e5da097801b98144294ed4cb
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-09-28 12:25:10 +0900

    chore: add page title, use vite env conventions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,10 @@
 <script>
-	// The favicon will likely be inlined in production.
+	// Small SVG files will likely be inlined in production.
 	// Reference https://github.com/vitejs/vite/issues/15986
-
-	// SVG favicons are not supported by Safari 18.
-	// Reference https://caniuse.com/link-icon-svg
-
 	import logo from '$lib/static/logo.svg';
+
+	import { page } from '$app/stores';
+	import { PUBLIC_SITE_NAME as SITE_NAME } from '$env/static/public';
 
 	let { children } = $props();
 </script>
@@ -15,6 +14,15 @@
 <!-- Use layout groups for further customizations. -->
 
 <svelte:head>
+	<!-- PageData values can be undefined despite being typed. -->
+	<!-- Blocked by https://github.com/sveltejs/kit/issues/11018 -->
+	<title>
+		{$page.data.pageTitle && $page.url.pathname !== '/' //
+			? `${$page.data.pageTitle} - ${SITE_NAME}`
+			: SITE_NAME}
+	</title>
+	<!-- SVG favicons are not supported by Safari 18. -->
+	<!-- Reference https://caniuse.com/link-icon-svg -->
 	<link rel="icon" href={logo} />
 	<script>
 		(function () {
