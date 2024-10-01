6c4fe68415ceb032d03bc6b478c0de5c3dc38e39
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-10-01 19:35:21 +0900

    refactor: pre-render all public routes

--- src/routes/(public)/unsupported/+page.ts
+++ src/routes/(public)/unsupported/+page.ts
@@ -2,11 +2,9 @@ import { dev } from '$app/environment';
 import type { PageLoad } from './$types.js';
 import { t } from './i18n.ts';
 
-export const prerender = true;
-
 // ES2023 might not be supported by legacy browsers.
 // Generate a plain HTML and CSS page by disabling CSR.
 // Reference https://kit.svelte.dev/docs/page-options#csr
 export const csr = dev;
 
-export const load = (() => ({ pageTitle: t.title })) satisfies PageLoad;
+export const load = (() => ({ pageTitle: t.pageTitle })) satisfies PageLoad;
--- src/routes/(public)/unsupported/i18n.ts
+++ src/routes/(public)/unsupported/i18n.ts
@@ -1,11 +1,11 @@
 const en = {
-	title: 'Unsupported Browser',
+	pageTitle: 'Unsupported Browser',
 	heading: 'Browser is not supported.',
 	message: 'Please use the latest browser for security and compatibility.'
 };
 
 const ko: typeof en = {
-	title: '지원되지 않는 브라우저',
+	pageTitle: '지원되지 않는 브라우저',
 	heading: '지원되지 않는 브라우저입니다.',
 	message: '보안 업데이트가 제공되는 최신 브라우저로 접속해 주세요.'
 };
