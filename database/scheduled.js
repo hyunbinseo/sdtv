1e462f14a5c9c1f1696940477c8223f1323b622f
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-10-30 21:06:23 +0900

    refactor: use better-sqlite3 backup

--- database/scheduled.js
+++ database/scheduled.js
@@ -1,10 +1,8 @@
 import { dateToSafeISOString } from '@hyunbinseo/tools';
 import { drizzle } from 'drizzle-orm/better-sqlite3';
-import { copyFileSync, mkdirSync } from 'node:fs';
-import { env, loadEnvFile } from 'node:process';
+import { mkdirSync } from 'node:fs';
+import { env } from 'node:process';
 import * as schema from '../src/lib/server/database/schema.js';
-
-loadEnvFile('.env.production');
 
 if (!env.SQLITE_PATH) throw new TypeError('SQLITE_PATH is undefined');
 
@@ -14,11 +12,6 @@ const db = drizzle({
 	schema
 });
 
-// Truncate WAL file to zero bytes before backup.
-// Reference https://www.sqlite.org/pragma.html#pragma_wal_checkpoint
-// Reference https://github.com/WiseLibs/better-sqlite3/issues/978
-db.$client.pragma('wal_checkpoint(TRUNCATE)');
-
 const path = import.meta.dirname + '/backups';
 mkdirSync(path, { recursive: true });
-copyFileSync(env.SQLITE_PATH, path + `/${dateToSafeISOString()}.sqlite`);
+await db.$client.backup(path + `/${dateToSafeISOString()}.sqlite`);
