0a48a42f374b5174a10f67c343b41c182c87e66a
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-10-31 10:13:48 +0900

    revert: remove handle-error hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,5 @@
-import { dev } from '$app/environment';
 import { VITE_LOCALE } from '$env/static/private';
-import type { Handle, HandleServerError } from '@sveltejs/kit';
-import { SqliteError } from 'better-sqlite3';
+import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const response = await resolve(event, {
@@ -19,15 +17,4 @@ export const handle: Handle = async ({ event, resolve }) => {
 	}
 
 	return response;
-};
-
-export const handleError: HandleServerError = ({ error }) => {
-	if (dev && error instanceof Error) console.error(error);
-	if (
-		error instanceof SqliteError &&
-		(error.code === 'SQLITE_CONSTRAINT_PRIMARYKEY' || error.code === 'SQLITE_CONSTRAINT_UNIQUE')
-	) {
-		// Randomly generated IDs can collide and cause constraint errors. (e.g. UUID, ULID)
-		// Since the likelihood is extremely low, the INSERT retry logic is not implemented.
-	}
 };
