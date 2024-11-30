2e56cf1a815023023c5de8cdde60880273127cc6
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-12-01 02:00:01 +0900

    chore: bump vite to v6

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,8 @@
 <script>
 	import { page } from '$app/stores';
 	import { PUBLIC_SITE_NAME } from '$env/static/public';
-	import logo from '$lib/static/logo.svg';
+	import logo from '$lib/static/logo.svg?no-inline';
 	import '../app.css';
-
-	// TODO Upgrade to Vite@6 and block favicon inlining.
-	// import logo from '$lib/static/logo.svg?no-inline';
 
 	// SVG favicons are not supported by Safari 18.
 	// Reference https://caniuse.com/link-icon-svg
