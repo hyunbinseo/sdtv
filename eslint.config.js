d4a07f3dd8ce6d9b04e72d8368cb5b55909fc56c
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-12-03 10:24:42 +0900

    chore: setup a pnpm monorepo

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import eslint from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+	eslint.configs.recommended,
+	...tseslint.configs.recommended,
+	prettier,
+	{
+		rules: {
+			'no-console': ['error', { allow: ['warn', 'error'] }],
+			'no-restricted-imports': ['error', 'assert', 'node:assert']
+		}
+	}
+);
