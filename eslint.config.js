6976ed9b55caccda1b5c36eaefc44b12d343c7b9
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-12-04 09:30:42 +0900

    fix: eslint console is not defined

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,20 @@
 import eslint from '@eslint/js';
 import prettier from 'eslint-config-prettier';
+import globals from "globals";
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
 	eslint.configs.recommended,
 	...tseslint.configs.recommended,
 	prettier,
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node
+			}
+		}
+	},
 	{
 		rules: {
 			'no-console': ['error', { allow: ['warn', 'error'] }],
