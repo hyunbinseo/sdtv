bd6c7a4d2770a079961225d8958ea9a1c1159ee1
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-12-06 15:16:02 +0900

    chore: add index on columns

--- packages/create-svelte-kitty/template/src/lib/server/database/schema/login.ts
+++ packages/create-svelte-kitty/template/src/lib/server/database/schema/login.ts
@@ -1,27 +1,33 @@
 import { generatePINString } from '@hyunbinseo/tools';
 import { randomUUID } from 'crypto';
 import { relations } from 'drizzle-orm';
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { index, integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 import { ulid } from 'ulid';
 import { ip } from '../columns.ts';
 import { loginExpiresIn, loginOtpLength } from '../config.ts';
 import { userTable } from './user.ts';
 
-export const loginTable = sqliteTable('login', {
-	id: text().primaryKey().$default(ulid),
-	userId: text()
-		.notNull()
-		.references(() => userTable.id),
-	code: text().notNull().$default(randomUUID),
-	otp: text()
-		.notNull()
-		.$default(() => generatePINString(loginOtpLength)),
-	expiresAt: integer({ mode: 'timestamp' })
-		.notNull()
-		.$default(() => new Date(Date.now() + loginExpiresIn)),
-	expiredAt: integer({ mode: 'timestamp' }),
-	ip
-});
+export const loginTable = sqliteTable(
+	'login',
+	{
+		id: text().primaryKey().$default(ulid),
+		userId: text()
+			.notNull()
+			.references(() => userTable.id),
+		code: text().notNull().$default(randomUUID),
+		otp: text()
+			.notNull()
+			.$default(() => generatePINString(loginOtpLength)),
+		expiresAt: integer({ mode: 'timestamp' })
+			.notNull()
+			.$default(() => new Date(Date.now() + loginExpiresIn)),
+		expiredAt: integer({ mode: 'timestamp' }),
+		ip
+	},
+	(table) => ({
+		userIdIdx: index('idx_login_user_id').on(table.userId)
+	})
+);
 
 export const loginRelations = relations(loginTable, ({ one }) => ({
 	user: one(userTable, { fields: [loginTable.userId], references: [userTable.id] })
